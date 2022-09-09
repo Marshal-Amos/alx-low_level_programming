2de7dc2223535f484dc4e562c06218ca13c33a32
Marshal Amos <amosmarshal@gmail.com>
2022-09-09 13:59:07 -0700

    Creating a program 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char alph;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+	}
+	for (alph = 'a'; alph < 'g'; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
