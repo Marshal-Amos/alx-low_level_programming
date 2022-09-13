f74e1956d617f9e080c035f98251e07979c54bb9
Marshal Amos <amosmarshal@gmail.com>
2022-09-13 14:00:58 -0700

    Creating a program 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two add integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
