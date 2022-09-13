802c6e6e434edf551ea6c00325a94ee7a66ac18b
Marshal Amos <amosmarshal@gmail.com>
2022-09-13 12:31:42 -0700

    Creating a program 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "6-abs.c"
+
+/**
+ * print_last_digit - a function that prints the last digit of a number
+ *
+ * Return: value of the last digit
+ */
+int print_last_digit(int c)
+{
+	_putchar('0' + _abs(c % 10));
+	return (_abs(c % 10));
+}
