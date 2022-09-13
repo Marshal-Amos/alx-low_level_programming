04e3e6a03d18227c733f5bfa88b584c9a9fd2daa
Marshal Amos <amosmarshal@gmail.com>
2022-09-13 10:49:25 -0700

    Creating the program 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabets
+ * in lower case
+ *
+ * Return: none
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int count;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchat('\n');
+	}
+}
