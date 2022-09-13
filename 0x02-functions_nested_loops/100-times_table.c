f0d68be11361513948722809a55d355bc3da2fa1
Marshal Amos <amosmarshal@gmail.com>
2022-09-13 16:42:23 -0700

    Correcting the program 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,10 +36,6 @@ void print_times_table(int n)
 					_putchar((product / 10) + '0');
 					_putchar((product % 10) + '0');
 				}
-				else if (product == 0)
-				{
-					_putchar(product + '0');
-				}
 				else
 				{
 					_putchar(((product / 100) % 10) + '0');
@@ -49,5 +45,10 @@ void print_times_table(int n)
 			}
 			_putchar('\n');
 		}
+	}
+	if (n == 0)
+	{
+		_putchar(n + '0');
+		_putchar('\n');
 	}
 }
