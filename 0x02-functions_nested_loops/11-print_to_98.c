5b84edd7e9ac26814814c42e1ece2d3d1fcb375f
Marshal Amos <amosmarshal@gmail.com>
2022-09-13 14:22:10 -0700

    Creating a program 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_to_98 - a function that prints all natural numbers
+ * starting from n to 98
+ *
+ * @n: the interger provided
+ *
+ * Return: none
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	printf("\n");
+}
