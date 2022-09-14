4f769e65e22ff5e6d903d72f68bc9ae34df1a479
Marshal Amos <amosmarshal@gmail.com>
2022-09-14 09:47:35 -0700

    Correcting the program 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - causes an infinite loop
  *
  * Return: 0
@@ -19,11 +18,11 @@ int main(void)
 	/**
 	 * while (i < 10)
 	 * {
-	 * 	 putchar(i);
+	 *	putchar(i);
 	 * }
 	 */
 
-        printf("Infinite loop avoided! \\o/\n");
-	
+	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
