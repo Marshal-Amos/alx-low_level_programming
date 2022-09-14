8bb1d32de97c3a0a9147955e5be1edae512be5f3
Marshal Amos <amosmarshal@gmail.com>
2022-09-13 18:15:43 -0700

    Creating the program 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Suuccess)
+ */
+int main(void)
+{
+	int num_1, num_2, fib_n, counter;
+
+	num_1 = 1;
+	num_2 = 2;
+	printf("%d, %d", num_1, num_2);
+
+	for (counter = 2; counter <= 98; counter++)
+	{
+		printf("%d", fib_n);
+
+		if (counter != 98)
+		{
+			printf(", ");
+			num_1 = num_2;
+			num_2 = fib_n;
+		}
+	}
+	printf("\n");
+	return (0);
+}
