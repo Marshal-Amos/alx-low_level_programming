a9afa8f91df35f4ee30982ee4bdfce5ebdd8970c
Marshal Amos <amosmarshal@gmail.com>
2022-09-14 15:57:16 -0700

    Correcting the program 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,9 +2,11 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
+ *
  * @a: first integer
  * @b: second integer
  * @c: third integer
+ *
  * Return: largest number
  */
 int largest_number(int a, int b, int c)
@@ -14,38 +16,15 @@ int largest_number(int a, int b, int c)
 	if (a > b && b > c)
 	{
 		largest = a;
-	} else if (a > b && c > b)
-	{
-		if (a > c)
-		{
-			largest = a;
-		} else
-		{
-			largest = c;
-		}
-	} else if (b > a && a > c)
+	}
+	else if (b > a && a > c)
 	{
 		largest = b;
-	} else if (b > a && c > a)
-	{
-		if (b > c)
-		{
-			largest = b;
-		} else
-		{
-			largest = c;
-		}
-	} else if (c > a && a > b)
+	}
+	else
 	{
 		largest = c;
-	} else if (c > a && b > a)
-	{
-		if (c > b)
-		{
-			largest = c;
-		} else
-		{
-			largest = b;
-		}
-	} return (largest);
+	}
+
+	return (largest);
 }
