7ca7db4ed5c9f168a7a5a0ce2acde1ae8b0da136
Marshal Amos <amosmarshal@gmail.com>
2022-09-14 09:51:25 -0700

    Correcting the program 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,15 +4,12 @@
  * main - causes an infinite loop
  *
  * Return: 0
- *
  */
-
 int main(void)
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
 
 	/**
@@ -23,6 +20,5 @@ int main(void)
 	 */
 
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
