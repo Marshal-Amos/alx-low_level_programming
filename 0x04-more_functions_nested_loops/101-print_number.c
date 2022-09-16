b9271e6791d331c3f75d707fa76b74f4c954ff3a
Marshal Amos <amosmarshal@gmail.com>
2022-09-16 16:46:52 -0700

    update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,14 +10,18 @@
 
 void print_number(int n)
 {
+	unsigned int number;
+
+	number = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		number = -number;
 	}
-	if ((n / 10) > 0)
+	if ((number / 10) > 0)
 	{
-		print_number(n / 10);
+		print_number(number / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((number % 10) + '0');
 }
