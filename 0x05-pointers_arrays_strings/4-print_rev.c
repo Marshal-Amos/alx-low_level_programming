c6a1996f58895174b721d33cb4dcf3ce5a7183c7
Marshal Amos <amosmarshal@gmail.com>
2022-09-19 11:03:46 -0700

    update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,7 +5,7 @@
  * print_rev - a function that prints a string, in reverse,
  * followed by a new line
  *
- * @s:  pointer
+ * @s: pointer
  *
  * Return: void
  */
@@ -14,7 +14,7 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = _strlen(s); i >= 0; i--)
+	for (i = (_strlen(s) - 1); i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
