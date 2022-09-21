c140aaf4d3b348fde2f04c6f462f7fd19217e3d5
Marshal Amos <amosmarshal@gmail.com>
2022-09-21 16:28:23 -0700

    Creating a program 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strcar - a function that concatenates two strings
+ *
+ * @dest: pointer
+ * @src: pointer
+ *
+ * Return: character
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		*(dest + (i + j)) = *(src + j);
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
