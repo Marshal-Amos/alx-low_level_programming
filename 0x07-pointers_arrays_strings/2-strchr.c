930fb6ffb8d8f6aa3d1b8a02f07dbab3bdab5be8
Marshal Amos <amosmarshal@gmail.com>
2022-09-26 10:06:03 -0700

    update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,7 +13,7 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -21,6 +21,10 @@ char *_strchr(char *s, char c)
 		{
 			return (s + i);
 		}
+	}
+	if (s[i] == c)
+	{
+		return (s + i);
 	}
 	return ('\0');
 }
