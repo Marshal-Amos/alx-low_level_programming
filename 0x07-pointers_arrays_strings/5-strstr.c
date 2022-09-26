99204aedd2a5e2688df007d47eb7dd5edcfa0896
Marshal Amos <amosmarshal@gmail.com>
2022-09-26 16:37:47 -0700

    update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,12 +15,27 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
+	int j;
 
+	if (needle[0] == '\0')
+	{
+		return (haystack);
+	}
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (haystack[i] == needle[i])
+		if (haystack[i] == needle[0])
 		{
-			return (needle);
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+				{
+					break;
+				}
+			}
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
 		}
 	}
 	return ('\0');
