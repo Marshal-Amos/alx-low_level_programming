9693b963d1da6bf637a835274224b57421e66be8
Marshal Amos <amosmarshal@gmail.com>
2022-09-30 07:40:01 -0700

    Creating a program: 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Argument counter
+ * @argv: Argument vector
+ *
+ * Return: Always 0 (Succcess)
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}
