dcce1fe6ef99102d128761b951ed4fea2dbabf32
Marshal Amos <amosmarshal@gmail.com>
2022-10-01 04:08:57 -0700

    Creating a file: 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Argument counter
+ * @argv: Argument vector
+ *
+ * Return: Always 0 (success)
+ * while 1 on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!isdigit(*argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += atoi(argv[i]);
+			}
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+	else 
+	{
+		printf("0\n");
+		return (0);
+	}
+}
