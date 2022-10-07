cd820102712eba83410ac21aeb115b6a5ea5349d
Marshal Amos <amosmarshal@gmail.com>
2022-10-06 17:05:53 -0700

    Update: 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -27,7 +27,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (strArray == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 	{
 		strArray[i] = 0;
 	}
