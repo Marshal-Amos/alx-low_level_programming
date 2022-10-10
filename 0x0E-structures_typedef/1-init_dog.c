fb16948cfdbaddac1548491b07515e741b7ade55
Marshal Amos <amosmarshal@gmail.com>
2022-10-10 09:40:44 -0700

    Creating a program: 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - a function that initialize a variable of type struct dog
+ *
+ * @d: structure
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
