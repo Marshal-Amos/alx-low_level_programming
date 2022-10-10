d58bd01bd7235271b8ece081c0e5756b9f7aa1b3
Marshal Amos <amosmarshal@gmail.com>
2022-10-10 15:48:09 -0700

    Creating a program: 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - a function that frees dogs
+ *
+ * @d: structure
+ *
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
