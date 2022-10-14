0f9c2f066a6c87715f33f63562c9eb232f37953c
Marshal Amos <amosmarshal@gmail.com>
2022-10-14 16:41:23 -0700

    Creating a file: 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: an integer
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*operator)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	operator = get_op_func(argv[2]);
+
+	if (*operator == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*argv[2] == '/' && b == 0) || (*argv[2] == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", operator(a, b));
+	return (0);
+}
