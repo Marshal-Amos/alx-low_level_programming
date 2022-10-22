9bfa6c9a2f0c6450063549669565aa97e416d131
Marshal Amos <amosmarshal@gmail.com>
2022-10-22 10:45:08 -0700

    Creating a file: 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * premain - a function that prints before calling the main function
+ *
+ * Return: nothing
+ */
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
