de243ed08823fbe697e36af6c0feadb80c5ce4f8
Marshal Amos <amosmarshal@gmail.com>
2022-10-25 12:02:30 -0700

    Creating a file: 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - a function that frees a listint_t list
+ *
+ * @head: pointer to the first node of the linked list
+ *
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temporary;
+
+	while (head != NULL)
+	{
+		temporary = head->next;
+		free(head);
+		head = temporary;
+	}
+}
