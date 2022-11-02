b7be16a4fbcca061607d7e56978780516a9c934b
Marshal Amos <amosmarshal@gmail.com>
2022-11-02 09:55:13 -0700

    Update: 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		write(fd, "", 1);
+		wrote = write(fd, "", 1);
+		if (wrote == -1)
+		{
+			return (-1);
+		}
 	}
 	else
 	{
