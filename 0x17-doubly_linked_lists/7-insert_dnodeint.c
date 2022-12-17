ddef79a88aba47ef94fd67ca69f119b9188c82b0
Marshal Amos <amosmarshal@gmail.com>
2022-12-17 14:47:08 -0800

    Update: 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,6 +23,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *h;
+		new->prev = NULL;
+		*h = new;
+		return (new);
+	}
+
 	for (i = 0; i < (idx - 1); i++)
 	{
 		if (hold == NULL)
@@ -31,7 +40,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 		hold = hold->next;
 	}
-	new->n = n;
 	new->prev = hold;
 	new->next = hold->next;
 	hold->next->prev = new;
